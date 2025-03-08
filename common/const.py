8f8694acc8157c38d38bd1e1df4db559622f2742
Ziheng <ziheng.yu1@gmail.com>
2025-03-09 00:54:01 +0800

    ali

--- common/const.py
+++ common/const.py
@@ -97,7 +97,7 @@ MODEL_LIST = [
               GEMINI, GEMINI_PRO, GEMINI_15_flash, GEMINI_15_PRO,GEMINI_20_flash_exp,
               CLAUDE_3_OPUS, CLAUDE_3_OPUS_0229, CLAUDE_35_SONNET, CLAUDE_35_SONNET_1022, CLAUDE_35_SONNET_0620, CLAUDE_3_SONNET, CLAUDE_3_HAIKU, "claude", "claude-3-haiku", "claude-3-sonnet", "claude-3-opus", "claude-3.5-sonnet",
               "moonshot-v1-8k", "moonshot-v1-32k", "moonshot-v1-128k",
-              QWEN, QWEN_TURBO, QWEN_PLUS, QWEN_MAX,QWEN_QWQ
+              QWEN, QWEN_TURBO, QWEN_PLUS, QWEN_MAX,QWEN_QWQ,
               LINKAI_35, LINKAI_4_TURBO, LINKAI_4o,
               DEEPSEEK_CHAT, DEEPSEEK_REASONER
             ]
